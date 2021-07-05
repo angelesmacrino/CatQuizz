677c983cfe69c2427a7f3bd74a18aaacd8c6514c
Angeles Macrino <63437504+angelesmacrino@users.noreply.github.com>
2021-07-05 15:20:09 -0300

    Update Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,17 +1,14 @@
 <template>
 <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-md-8 d-flex flex-column align-items-center">
+            <div class="col d-flex flex-column align-items-center justify-content-center">
                 <div class="row" id="question">QUESTION</div>
-                <div class="row answer" id="answer1">answer1</div>
-                <div class="row answer" id="answer2">answer2</div>
-                <div class="row answer" id="answer3">answer3</div>
-                <div class="row answer" id="answer4">answer4</div>
-                <div class="row answer" id="answer5">answer5</div>
+                    <div class="row answer" id="answer1">answer1</div>
+                    <div class="row answer" id="answer2">answer2</div>
+                    <div class="row answer" id="answer3">answer3</div>
+                    <div class="row answer" id="answer4">answer4</div>
+                    <div class="row answer" id="answer5">answer5</div>
                 <div class="row align-self-start" id="score">SCORE:</div>
-            </div>
-            <div class="col-sm-6 col-md-4 d-flex">
-                <img :src="require('../assets/' + catProfessor.Img)" :alt="catProfessor.alt" class="img-fluid align-self-center"  v-show="!mobileView">
             </div>
         </div>
 </div>
@@ -19,38 +16,69 @@
 
 <script>
 export default {
-    data: function() {
-            return {
-                catProfessor: {
-                Img: "cat.svg",
-                alt: "Illustration of a Cat with glasses and a graduation cap"
-                },
-                mobileView: true
-            }
-        },
-    methods: {
-        handleView() {
-            this.mobileView = window.innerWidth <= 600;
-            console.log(window.innerWidth)
-      }
-    },
-    created() {
-      this.handleView();
-      window.addEventListener('resize', this.handleView)
-  }
+   
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+$mobile-width: 600px;
+$tablet-width: 900px;
+$desktop-width: 1200px;
+$large-desktop-width: 1800px;
+
+@mixin mobile {
+  @media (max-width: #{$mobile-width}) {
+    @content;
+  }
+}
+
+@mixin s-tablet {
+    @media (min-width: #{$mobile-width + 1px}) and (max-width: #{$tablet-width}) {
+    @content;
+  }
+}
+
+@mixin l-tablet {
+    @media (min-width: #{$tablet-width + 1px}) and (max-width: #{$desktop-width}) {
+    @content;
+  }
+}
+
+@mixin desktop {
+    @media (min-width: #{$desktop-width + 1px}) and (max-width: #{$large-desktop-width}) {
+    @content;
+  }
+}
+
+@mixin l-desktop {
+    @media (min-width: #{$large-desktop-width + 1px}){
+    @content;
+  }
+}
+
+
 .container {
-    margin:5px;
+    @media (orientation:portrait) {
+        margin-top:20%;
+    }
     #question {
     background-color: white;
     border-radius: 25px;
     padding:10px;
     font-family: 'Fredoka One', cursive;
-    font-size: 2rem;
     color:#073B4C;
+        @include mobile {
+            font-size: 3rem;
+        }
+        @include s-tablet {
+            font-size: 3.5rem;
+        }
+        @include l-tablet {
+            font-size: 5rem;
+        }
+        @include desktop {
+            font-size: 5.5rem;
+        }
     }
     .answer {
     background-color: white;
@@ -60,6 +88,18 @@ export default {
     font-size: 1.5rem;
     margin:5px;
     color:#42565d;
+        @include mobile {
+            font-size: 2rem;
+        }
+        @include s-tablet {
+            font-size: 2.5rem;
+        }
+        @include l-tablet {
+            font-size: 4rem;
+        }
+        @include desktop {
+            font-size: 4.5rem;
+        }
     }
     #score {
     margin-top: 20px;
@@ -68,39 +108,7 @@ export default {
     font-size: 2rem;
     color:#073B4C;
     }
-}
-@media (max-width:540px) {
-        .container {
-            margin-top:25px
-        }
     }
-@media (min-width:768px) and (max-width:1024px) and (orientation:portrait) {
-        .container {
-            margin-top:40%;
-            #question {
-                font-size: 4rem;
-            }
-            .answer {
-                font-size: 3rem;
-                }
-            #score {
-                font-size: 2rem;
-                }
-        }
-    }
-
-@media (min-width:1024px) and (max-width:1366px) and (orientation:landscape) {
-    .container {
-        margin-top:10%;
-    #question {
-                font-size: 5rem;
-            }
-            .answer {
-                font-size: 4rem;
-                }
-            #score {
-                font-size: 3rem;
-                }
-    }
-}
+    
+ 
 </style>
